7c1924bbc4dd4f1e6a420d42767e9b3d956cd415
merry <k.maria.m.24.05@gmail.com>
2024-11-19 09:08:25 +0200

    create table

--- src/app/(landing)/auction-list/page.tsx
+++ src/app/(landing)/auction-list/page.tsx
@@ -1,25 +1,68 @@
+import Image from "next/image";
+import { format, parseISO } from "date-fns";
+import { baseUrl, auctionListEndpoint } from "@/constants/apiEndpoints";
+import styles from "./list.module.scss";
+
 interface Props {
   className?: string;
 }
 
-const baseUrl = "http://localhost:8000/";
-const auctionListEndpoint = "auctions/";
+const columns = ["ID", "Name", "Image", "Start At"];
+const IMAGE_DIMENSIONS = {
+  height: 200,
+  width: 200,
+};
 
 export default async function AuctionListPage({ className }: Props) {
   const data = await fetch(`${baseUrl}${auctionListEndpoint}`);
+
   const auctionData = await data.json();
   const { results } = auctionData;
 
-  console.log("Log from server component");
-
-  // console.log(auctionList);
   return (
     <section className={className}>
-      <ul>
-        {results.map((auctionItem) => (
-          <li key={auctionItem.id}>{auctionItem.lot.name}</li>
-        ))}
-      </ul>
+      <h1 className={styles.title}>Auction List</h1>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {columns.map((column) => (
+              <th className={styles.head} key={column}>
+                {column}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((item) => {
+            const lot = item.lot || {};
+            const startAt = lot.start_at;
+            const formattedDate = startAt
+              ? format(parseISO(startAt), "MMMM dd, yyyy, HH:mm:ss OOOO")
+              : "No start date";
+
+            return (
+              <tr key={item.id || lot.id}>
+                <td>{lot.id || "No ID"}</td>
+                <td>{lot.name || "No Name"}</td>
+                <td>
+                  {lot.image ? (
+                    <Image
+                      priority
+                      alt={lot.name || "Auction Image"}
+                      src={lot.image}
+                      height={IMAGE_DIMENSIONS.height}
+                      width={IMAGE_DIMENSIONS.width}
+                    />
+                  ) : (
+                    "No image"
+                  )}
+                </td>
+                <td>{formattedDate}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </section>
   );
 }
